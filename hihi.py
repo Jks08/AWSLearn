9c1504c363e6803b6627ac57bec4ac292abe1343
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-05-02 14:55:45 +0530

    added SQS queue creation logic

--- hihi.py
+++ hihi.py
@@ -1,14 +1,78 @@
 #!/usr/bin/python3
 import sys
-print("Environment: " + sys.argv[1])
-print("Queue Name: " + sys.argv[2])
-print("Dead Letter Queue Name: " + sys.argv[3])
-print("Max Receive Count: " + sys.argv[4])
-print("LOB: " + sys.argv[5])
-print("REF_ID: " + sys.argv[6])
-print("Application Name: " + sys.argv[7])
-print("SNS Topic Name: " + sys.argv[8])
-print("SNS Subscription Required: " + sys.argv[9])
+import boto3
+
+Environment = sys.argv[1]
+QueueName = sys.argv[2]
+DeadLetterQueueName = sys.argv[3]
+MaxReceiveCount = sys.argv[4]
+LOB = sys.argv[5]
+REF_ID = sys.argv[6]
+ApplicationName = sys.argv[7]
+SNSTopicName = sys.argv[8]
+SNSSubscriptionRequired = sys.argv[9]
+
+# Now, we provision the SQS Queue and SNS Topic(if required) for the application using boto3
+# Some Rules:
+# 1. If SNSSubscriptionRequired is True, then we create a SNS Topic and subscribe the SQS Queue to it
+# 2. If SNSSubscriptionRequired is False, then we create a SQS Queue and do not create a SNS Topic
+# 3. If there is no value for DeadLetterQueueName, then we do not create a Dead Letter Queue and we do not include a RedrivePolicy in the SQS Queue
+# 4. If there is a value for DeadLetterQueueName, then we create a Dead Letter Queue and we include a RedrivePolicy in the SQS Queue
+
+# Create SQS client
+sqs = boto3.client('sqs')
+
+# Create SNS client
+sns = boto3.client('sns')
+
+# Create SQS Queue
+if DeadLetterQueueName == "":
+    response = sqs.create_queue(
+        QueueName=QueueName,
+        Tags=[
+            {
+                'Key': 'LOB',
+                'Value': LOB
+            },
+            {
+                'Key': 'REF_ID',
+                'Value': REF_ID
+            },
+            {
+                'Key': 'Application Name',
+                'Value': ApplicationName
+            }
+        ]
+    )
+else:
+    response = sqs.create_queue(
+        QueueName=QueueName,
+        Tags=[
+            {
+                'Key': 'LOB',
+                'Value': LOB
+            },
+            {
+                'Key': 'REF_ID',
+                'Value': REF_ID
+            },
+            {
+                'Key': 'Application Name',
+                'Value': ApplicationName
+            }
+        ],
+        Attributes={
+            'RedrivePolicy': '{"deadLetterTargetArn":"Fn::GetAtt": ["' + DeadLetterQueueName + '","Arn"],"maxReceiveCount":"' + MaxReceiveCount + '"}'
+        }
+    )
+    # Create Dead Letter Queue
+    response = sqs.create_queue(
+        QueueName=DeadLetterQueueName,
+        # No tags required for Dead Letter Queue
+    )
+
+
+
 
 # Print the number of arguments
-print(len(sys.argv))
+# print(len(sys.argv))
