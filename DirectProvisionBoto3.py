739406e6c2c2bc8501d75f4b2dcf84cba888a6a7
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-05-03 12:08:45 +0530

    got the sns topic and subscription right

--- DirectProvisionBoto3.py
+++ DirectProvisionBoto3.py
@@ -19,20 +19,21 @@ except IndexError:
 
 sqs = boto3.client('sqs')
 sns = boto3.client('sns')
+queue_arn = None
 
-# Now, we provision the SQS Queue and SNS Topic(if required) for the application using boto3
-# Some Rules:
-# 1. If SNSSubscriptionRequired is True, then we create a SNS Topic and subscribe the SQS Queue to it
-# 2. If SNSSubscriptionRequired is False, then we create a SQS Queue and do not create a SNS Topic
-# 3. If there is no value for DeadLetterQueueName, then we do not create a Dead Letter Queue and we do not include a RedrivePolicy in the SQS Queue
-# 4. If there is a value for DeadLetterQueueName, then we create a Dead Letter Queue and we include a RedrivePolicy in the SQS Queue
+
 def provision_sqs_sns_queue():
+    global queue_arn
     # If Queue already exists, then we print the Queue URL and exit
     for url in sqs.list_queues()['QueueUrls']:
         if QueueName in url:
+            queue_attributes = sqs.get_queue_attributes(
+                QueueUrl=url,
+                AttributeNames=['QueueArn']
+                )
+            queue_arn = queue_attributes['Attributes']['QueueArn']
             print(f"Queue already exists!")
-            print(f"Queue URL is {url}")
-            sys.exit(0)
+            return url, queue_arn
 
     try:
         if DeadLetterQueueName == "":
@@ -80,6 +81,11 @@ def provision_sqs_sns_queue():
         sys.exit(1)
 
 def provision_sns_topic():
+    for topic in sns.list_topics()['Topics']:
+        if SNSTopicName in topic['TopicArn']:
+            print(f"SNS Topic already exists!")
+            return topic['TopicArn']
+        
     try:
         if SNSSubscriptionRequired == "True":
             response = sns.create_topic(
@@ -87,7 +93,7 @@ def provision_sns_topic():
                 Attributes={
                     'DisplayName': SNSTopicName
                 },
-                tags=[
+                Tags=[
                     {
                         'Key': 'LOB',
                         'Value': LOB
@@ -106,30 +112,54 @@ def provision_sns_topic():
             print(f"SNS Topic created: {topic_arn}")
 
             # Subscribe SQS Queue to SNS Topic
+            global queue_arn
             response = sns.subscribe(
                 TopicArn=topic_arn,
                 Protocol='sqs',
-                Endpoint=QueueName
+                Endpoint=queue_arn 
             )
+
             subscription_arn = response['SubscriptionArn']
             print(f"SQS Queue subscribed to SNS Topic: {subscription_arn}")
 
         else:
-            print("No SNS Topic created")
+            response = sns.create_topic(
+                Name=SNSTopicName,
+                Attributes={
+                    'DisplayName': SNSTopicName
+                },
+                Tags=[
+                    {
+                        'Key': 'LOB',
+                        'Value': LOB
+                    },
+                    {
+                        'Key': 'REF_ID',
+                        'Value': REF_ID
+                    },
+                    {
+                        'Key': 'Application Name',
+                        'Value': ApplicationName
+                    }
+                ]
+            )
+            topic_arn = response['TopicArn']
+            print(f"SNS Topic created: {topic_arn}")
+
     except Exception as e:
-        print(f"Error is {e}")
+        print(f"Error: {e}")
         sys.exit(1)
 
-if QueueName == "":
-    print("Please provide a value for QueueName")
-    sys.exit(1)
+if QueueName != "":
+    print("Creating SQS Queue")
+    print(provision_sqs_sns_queue())
 
-else:
-    provision_sqs_sns_queue()
+if SNSSubscriptionRequired == "False":
+    print("SNS Subscription not required")
 
 if SNSTopicName == "":
-    print("Please provide a value for SNSTopicName")
-    sys.exit(1)
+    print("Not creating SNS Topic as SNSTopicName is not provided")
 
 else:
-    provision_sns_topic()
+    print("Creating SNS Topic")
+    print(provision_sns_topic())
