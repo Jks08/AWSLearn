75f95a505706639cd6b536584a8a79573ebf939a
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-05-04 16:57:45 +0530

    playing around with pipeline script

--- sqssnsPipeline.groovy
+++ sqssnsPipeline.groovy
@@ -29,6 +29,11 @@ pipeline{
                     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]){
                         sh "curl https://raw.githubusercontent.com/jks08/AWSLearn/master/DirectProvisionCloudFormation.py -o DirectProvisionCloudFormation.py"
                         sh "python3 DirectProvisionCloudFormation.py ${params.ENVIRONMENT} ${params.QUEUE_NAME} ${params.DeadLetter_Queue_Name} ${params.maxReceiveCount} ${params.LOB} ${params.REF_ID} ${params.Application_Name} ${params.SNS_TOPIC_NAME} ${params.SNS_SUBSCRIPTION_REQUIRED} ${params.StackName}"
+                        // Get the logs that occur in AWS because of the above script
+                        sh "aws cloudformation describe-stack-events --stack-name ${params.StackName} --region ${params.REGION} --output text --query 'StackEvents[*].[ResourceStatus,ResourceStatusReason]'"
+                        // If there is any error while updating/creating the stack, show job as failed and the error message
+                        sh "aws cloudformation describe-stack-events --stack-name ${params.StackName} --region ${params.REGION} --output text --query 'StackEvents[*].[ResourceStatus,ResourceStatusReason]' | grep -i 'FAILED'"
+                        
                     }
             }
         }
