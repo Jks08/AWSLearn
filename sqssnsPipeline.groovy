8a8cbb53aa95d9314f1d6c61d774b10b65164cab
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-05-04 21:42:07 +0530

    comment out shell cmd of logs

--- sqssnsPipeline.groovy
+++ sqssnsPipeline.groovy
@@ -30,7 +30,7 @@ pipeline{
                         sh "curl https://raw.githubusercontent.com/jks08/AWSLearn/master/DirectProvisionCloudFormation.py -o DirectProvisionCloudFormation.py"
                         sh "python3 DirectProvisionCloudFormation.py ${params.ENVIRONMENT} ${params.QUEUE_NAME} ${params.DeadLetter_Queue_Name} ${params.maxReceiveCount} ${params.LOB} ${params.JIRA_ID} ${params.Application_Name} ${params.SNS_TOPIC_NAME} ${params.SNS_SUBSCRIPTION_REQUIRED} ${params.VisibilityTimeout} ${params.MessageRetentionPeriod} ${params.MaximumMessageSize} ${params.DelaySeconds} ${params.ReceiveMessageWaitTimeSeconds} ${params.RawMessageDelivery} ${params.StackName}"
                         // Get the logs that occur in AWS because of the above script
-                        sh "aws cloudformation describe-stack-events --stack-name ${params.StackName} --output text --query 'StackEvents[*].[ResourceStatus,ResourceStatusReason]'"
+                        // sh "aws cloudformation describe-stack-events --stack-name ${params.StackName} --output text --query 'StackEvents[*].[ResourceStatus,ResourceStatusReason]'"
                         // If there is any error while updating/creating the stack, show job as failed and the error message and exit the pipeline
                         // sh "aws cloudformation describe-stack-events --stack-name ${params.StackName} --output text --query 'StackEvents[*].[ResourceStatus,ResourceStatusReason]' | grep -i 'FAILED' && exit 1 || echo 'No Errors'"
 
