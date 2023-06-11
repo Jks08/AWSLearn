6bff06a53284d3b6511db6e69fb443f6448b8b31
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2023-06-11 16:07:31 +0530

    Create ami_data.py

---
+++ ami_data.py
@@ -0,0 +1,31 @@
+# !/usr/bin/python3
+import boto3
+import csv
+
+session = boto3.Session(profile_name='default')
+
+ec2_resource = session.resource('ec2')
+ec2_client = session.client('ec2')
+
+# amis = ec2_client.describe_images(Owners=['self'])
+amis = ec2_resource.images.filter(Owners=['self'])
+
+scraped_data = []
+
+for ami in amis:
+    ami_id = ami.id
+    block_device_mappings = ami.block_device_mappings
+    for block_device in block_device_mappings:
+        device_id = block_device['DeviceName']
+        
+        # Retrieve snapshot information
+        snapshot_id = block_device['Ebs']['SnapshotId']
+        
+        # Describe the snapshot to get DeleteOnTermination attribute
+        snapshot = ec2_client.describe_snapshots(SnapshotIds=[snapshot_id])['Snapshots'][0]
+        delete_on_termination = snapshot['DeleteOnTermination']
+        
+        # Store the scraped data in a list
+        scraped_data.append([ami_id, device_id, delete_on_termination])
+
+print(scraped_data)
