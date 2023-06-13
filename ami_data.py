a0a1b9f066cad63e67af267c55390e9b364d54b8
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2023-06-13 16:07:28 +0530

    Update ami_data.py

--- ami_data.py
+++ ami_data.py
@@ -3,9 +3,7 @@ import csv
 
 rds = boto3.client('rds')
 dbs = rds.describe_db_instances()
+rds = boto3.client('rds')
+dbs = rds.describe_db_instances()
 for db in dbs['DBInstances']:
-    print(db['DBInstanceIdentifier'])
-    
-for db in dbs['DBInstances']:
-    print(db['DBInstanceIdentifier'])
-    print(db['PreferredMaintenanceWindow'])
+    print(f"{db['DBInstanceIdentifier']} | {db['PreferredMaintenanceWindow']}")
