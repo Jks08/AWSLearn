828174cd0b90aaf452e4bd2870457b60f424dbd6
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2023-06-13 16:05:21 +0530

    Update ami_data.py

--- ami_data.py
+++ ami_data.py
@@ -1,31 +1,11 @@
-# !/usr/bin/python3
 import boto3
 import csv
 
-session = boto3.Session(profile_name='default')
-
-ec2_resource = session.resource('ec2')
-ec2_client = session.client('ec2')
-
-# amis = ec2_client.describe_images(Owners=['self'])
-amis = ec2_resource.images.filter(Owners=['self'])
-
-scraped_data = []
-
-for ami in amis:
-    ami_id = ami.id
-    block_device_mappings = ami.block_device_mappings
-    for block_device in block_device_mappings:
-        device_id = block_device['DeviceName']
-        
-        # Retrieve snapshot information
-        snapshot_id = block_device['Ebs']['SnapshotId']
-        
-        # Describe the snapshot to get DeleteOnTermination attribute
-        snapshot = ec2_client.describe_snapshots(SnapshotIds=[snapshot_id])['Snapshots'][0]
-        # delete_on_termination = snapshot['DeleteOnTermination']
-        print(dir(snapshot))
-        # Store the scraped data in a list
-        # scraped_data.append([ami_id, device_id, delete_on_termination])
-
-# print(scraped_data)
+rds = boto3.client('rds')
+dbs = rds.describe_db_instances()
+for db in dbs['DBInstances']:
+    print(db['DBInstanceIdentifier'])
+    
+for db in dbs['DBInstances']:
+    print(db['DBInstanceIdentifier'])
+    print(db['PreferredMaintenanceWindow'])
