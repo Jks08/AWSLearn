aec89238ac9d5da67174690c27839ba15ca978bb
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2023-06-13 16:32:12 +0530

    Update ami_data.py

--- ami_data.py
+++ ami_data.py
@@ -4,10 +4,12 @@ import csv
 rds = boto3.client('rds')
 dbs = rds.describe_db_instances()
 for db in dbs['DBInstances']:
-    print(f"{db['DBInstanceIdentifier']}")
-    print(f" Backup Retention Perios: {db['BackupRetentionPeriod']}")
-    print(f" Latest Restorable Time: {db['LatestRestorableTime']}")
-    print(f" Copy Tags to Snapshots: {db['CopyTagsToSnapshot']}")
-    print(f" Backup Window: {db['PreferredBackupWindow']}")
-    print("\n")
-
+    try:
+        print(f"{db['DBInstanceIdentifier']}")
+        print(f" Backup Retention Perios: {db['BackupRetentionPeriod']}")
+        print(f" Latest Restorable Time: {db['LatestRestorableTime']} in UTC")
+        print(f" Copy Tags to Snapshots: {db['CopyTagsToSnapshot']}")
+        print(f" Backup Window: {db['PreferredBackupWindow']} in IST")
+        print("\n")
+    except KeyError:
+        pass
