409565e0cfb2e5cf68ac730190973d5dbc72d94e
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2023-06-13 16:35:37 +0530

    Update ami_data.py

--- ami_data.py
+++ ami_data.py
@@ -6,10 +6,27 @@ dbs = rds.describe_db_instances()
 for db in dbs['DBInstances']:
     try:
         print(f"{db['DBInstanceIdentifier']}")
+    except:
+        print("No DB Instances found")
+        continue
+    try:
         print(f" Backup Retention Perios: {db['BackupRetentionPeriod']}")
+    except:
+        print("No Backup Retention Period found")
+        continue
+    try:
         print(f" Latest Restorable Time: {db['LatestRestorableTime']} in UTC")
+    except:
+        print("No Latest Restorable Time found")
+        continue
+    try:
         print(f" Copy Tags to Snapshots: {db['CopyTagsToSnapshot']}")
+    except:
+        print("No Copy Tags to Snapshots found")
+        continue
+    try:
         print(f" Backup Window: {db['PreferredBackupWindow']} in IST")
-        print("\n")
-    except KeyError:
-        pass
+    except:
+        print("No Backup Window found")
+        continue
+    print("\n")
